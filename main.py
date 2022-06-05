991b67ba63f3149c9c8ac7297a0c88dc8d047d6b
Alianur Rahman <71885945+AlianurRahman@users.noreply.github.com>
2022-06-05 18:31:33 +0700

    project initial & create data visualization & exploration function
    plot waveplots and spectograms for audio signals.

---
+++ main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+
+import os
+import sys
+
+# librosa is a Python library for analyzing audio and music. It can be used to extract the data from the audio files we will see it later.
+import librosa
+import librosa.display
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+
+# to play the audio files
+from IPython.display import Audio
+
+import keras
+from keras.callbacks import ReduceLROnPlateau
+from keras.models import Sequential
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
+
+import warnings
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+def create_waveplot(data, sr, e):
+    plt.figure(figsize=(10, 3))
+    plt.title('Waveplot for audio with {} emotion'.format(e), size=15)
+    librosa.display.waveplot(data, sr=sr)
+    plt.show()
+
+def create_spectrogram(data, sr, e):
+    # stft function converts the data into short term fourier transform
+    X = librosa.stft(data)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(12, 3))
+    plt.title('Spectrogram for audio with {} emotion'.format(e), size=15)
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')   
+    #librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar()
+
+
+
+# taking any example and checking for techniques.
+path = np.array(data_path.Path)[1]
+data, sample_rate = librosa.load(path)
