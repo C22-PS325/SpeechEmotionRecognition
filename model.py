a8a5fffb5bce277f17bd5bdce63c6511f16236a8
Alianur Rahman <71885945+AlianurRahman@users.noreply.github.com>
2022-06-07 16:02:53 +0700

    Refactoring model.py
    divide model.py into separate file (exploration.py, extraction.py, augmentation.py) for modulation

--- model.py
+++ model.py
@@ -4,125 +4,34 @@ import numpy as np
 import os
 import sys
 
-# librosa is a Python library for analyzing audio and music. It can be used to extract the data from the audio files we will see it later.
+# librosa is a Python library for analyzing audio and music. It can be used to extract the data from the audio files.
 import librosa
 import librosa.display
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 
 # to play the audio files
-from IPython.display import Audio
-
-import keras
 from keras.callbacks import ReduceLROnPlateau
 from keras.models import Sequential
 from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization
-from tensorflow.keras.utils import to_categorical
-from keras.callbacks import ModelCheckpoint
 
 import warnings
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-def create_waveplot(data, sr, e):
-    plt.figure(figsize=(10, 3))
-    plt.title('Waveplot for audio with {} emotion'.format(e), size=15)
-    librosa.display.waveplot(data, sr=sr)
-    plt.show()
+#import exploration.py
 
-def create_spectrogram(data, sr, e):
-    # stft function converts the data into short term fourier transform
-    X = librosa.stft(data)
-    Xdb = librosa.amplitude_to_db(abs(X))
-    plt.figure(figsize=(12, 3))
-    plt.title('Spectrogram for audio with {} emotion'.format(e), size=15)
-    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')   
-    #librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
-    plt.colorbar()
-
-
-
-def noise(data):
-    noise_amp = 0.035*np.random.uniform()*np.amax(data)
-    data = data + noise_amp*np.random.normal(size=data.shape[0])
-    return data
-
-def stretch(data, rate=0.8):
-    return librosa.effects.time_stretch(data, rate)
-
-def shift(data):
-    shift_range = int(np.random.uniform(low=-5, high = 5)*1000)
-    return np.roll(data, shift_range)
-
-def pitch(data, sampling_rate, pitch_factor=0.7):
-    return librosa.effects.pitch_shift(data, sampling_rate, pitch_factor)
+#import augmentation.py
 
 # taking any example and checking for techniques.
 path = np.array(data_path.Path)[1]
 data, sample_rate = librosa.load(path)
 
-#Feature Extraction
-
-def extract_features(data):
-    # ZCR
-    result = np.array([])
-    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
-    result=np.hstack((result, zcr)) # stacking horizontally
-
-    # Chroma_stft
-    stft = np.abs(librosa.stft(data))
-    chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, chroma_stft)) # stacking horizontally
-
-    # MFCC
-    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, mfcc)) # stacking horizontally
-
-    # Root Mean Square Value
-    rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
-    result = np.hstack((result, rms)) # stacking horizontally
-
-    # MelSpectogram
-    mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, mel)) # stacking horizontally
-    
-    return result
-
-def get_features(path):
-    # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
-    data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
-    
-    # without augmentation
-    res1 = extract_features(data)
-    result = np.array(res1)
-    
-    # data with noise
-    noise_data = noise(data)
-    res2 = extract_features(noise_data)
-    result = np.vstack((result, res2)) # stacking vertically
-    
-    # data with stretching and pitching
-    new_data = stretch(data)
-    data_stretch_pitch = pitch(new_data, sample_rate)
-    res3 = extract_features(data_stretch_pitch)
-    result = np.vstack((result, res3)) # stacking vertically
-    
-    return result
-
-X, Y = [], []
-for path, emotion in zip(data_path.Path, data_path.Emotions):
-    feature = get_features(path)
-    for ele in feature:
-        X.append(ele)
-        # appending emotion 3 times as we have made 3 augmentation techniques on each audio file.
-        Y.append(emotion)
-
-len(X), len(Y), data_path.Path.shape
+# import extraction.py
 
 Features = pd.DataFrame(X)
 Features['labels'] = Y
